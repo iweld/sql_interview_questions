d9852ea9ef2eb6cc8998cb834780d2b876e5696b
Jaime M. Shaker <eyeweld@gmail.com>
2022-11-27 09:05:49 -0600

    Adding sql query file

---
+++ sql_query.sql
@@ -0,0 +1,16 @@
+/*
+	Common entry level SQL interview questions and answers.
+	These questions appear in many interview preperation resources
+	found online.
+*/
+
+1- How to find duplicates in a table
+2- How to delete duplicates from a table
+3- Difference between union and union all
+4- Difference between rank,row_number and dense_rank
+5- Find records in a table which are not present in another table
+6- Find second highest salary employees in each department
+7- Find employees with salary more than their manager's salary
+8- Difference between inner and left join
+9- update a table and swap gender values.
+10- Number of records in output with different kinds of join.
